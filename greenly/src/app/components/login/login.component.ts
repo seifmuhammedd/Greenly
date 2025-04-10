c42bdb6903661bea1b05e0bcb5dd9cee3735666d
seifmuhammedd <seifamuhammed2@gmail.com>
2025-04-10 21:25:23 +0200

    log in design done

--- greenly/src/app/components/login/login.component.ts
+++ greenly/src/app/components/login/login.component.ts
@@ -1,12 +1,21 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor( private _FormBuilder: FormBuilder, private _Router: Router ) {}
+
+  loginForm: FormGroup = this._FormBuilder.group({
+    email: [null,[ Validators.required, Validators.email]],
+    password: [null, [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)]]
+  })
 }
