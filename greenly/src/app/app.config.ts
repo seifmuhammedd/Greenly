1cdfc4ec2858901063f9a4da785a56d45c247c6c
seifmuhammedd <seifamuhammed2@gmail.com>
2025-04-10 17:59:01 +0200

    home design done

--- greenly/src/app/app.config.ts
+++ greenly/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions()), provideClientHydration()]
+  providers: [
+    provideRouter(routes, withViewTransitions()),
+    provideClientHydration(),
+    importProvidersFrom(BrowserAnimationsModule)
+  ],
 };
--- greenly/src/app/components/home/home.component.ts
+++ greenly/src/app/components/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  mainSlider: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 1500,
+    navText: ['', ''],
+    items: 1,
+    nav: false
+  }
 
 }
