53e3d44b124f8d539c94ff386a9fbd76756e4e5e
seifmuhammedd <seifamuhammed2@gmail.com>
2025-04-29 18:20:52 +0300

    .

--- greenly/src/app/components/cart/cart.component.ts
+++ greenly/src/app/components/cart/cart.component.ts
@@ -44,7 +44,6 @@ export class CartComponent implements OnInit {
           closeButton: true,
         });
         this.cartData=res
-        console.log(this.cartData.products)
       },
       error: (err)=>{
         console.log(err.error.message)
