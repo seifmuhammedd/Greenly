476dc0b381f2e4db4858b2d3e968dfd027cb6047
seifmuhammedd <seifamuhammed2@gmail.com>
2025-05-15 21:10:45 +0300

    ..

--- greenly/src/app/components/navbar/navbar.component.ts
+++ greenly/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { CartService } from '../../core/services/cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -11,15 +12,15 @@ import { CartService } from '../../core/services/cart.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit , OnDestroy {
   
   private readonly _PLATFORM_ID = inject(PLATFORM_ID);
   private readonly _Router = inject(Router)
   public readonly _AuthService = inject(AuthService)
   public readonly _CartService = inject(CartService)
 
-  cartCounter : number = 0
-
+  cartCounter !: number 
+  counterSub !: Subscription
   ngOnInit(): void {
     if(isPlatformBrowser(this._PLATFORM_ID)){
       if(localStorage.getItem("userToken")){
@@ -31,7 +32,7 @@ export class NavbarComponent implements OnInit {
       }
     }
 
-    this._CartService.cartCounter.subscribe({
+    this.counterSub =  this._CartService.cartCounter.subscribe({
       next: (value) => this.cartCounter = value
     })
     
@@ -41,6 +42,10 @@ export class NavbarComponent implements OnInit {
     localStorage.removeItem("userToken")
     this._AuthService.isLoggedIn.set(false)
     this._Router.navigate(["/app/system/login"])
+  }
+
+  ngOnDestroy(): void {
+    this.counterSub?.unsubscribe()
   }
 
 }
