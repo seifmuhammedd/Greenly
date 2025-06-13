5b887bd844e17e0ad294420e55ad200b2b53eaa2
seifmuhammedd <seifamuhammed2@gmail.com>
2025-06-13 05:18:49 +0300

    licence errors hndeled

--- greenly/src/app/components/apiary-licence/apiary-licence.component.ts
+++ greenly/src/app/components/apiary-licence/apiary-licence.component.ts
@@ -1,38 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { LicenceService } from '../../core/services/licence.service';
 import { NgClass } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-apiary-licence',
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule, NgClass],
   templateUrl: './apiary-licence.component.html',
-  styleUrl: './apiary-licence.component.css',
+  styleUrls: ['./apiary-licence.component.css'],
 })
 export class ApiaryLicenceComponent implements OnInit {
-
-  constructor(private _FormBuilder: FormBuilder, private _LicenceService: LicenceService) { }
-
   charactersLeft: number = 3000;
 
   apiaryForm: FormGroup = this._FormBuilder.group({
     fullName: [null, [Validators.required]],
-    phoneNumber: [null, [Validators.required]],
+    phoneNumber: [null, [Validators.required,Validators.pattern(/^(\+201|01|00201)[0-2,5]{1}[0-9]{8}$/)]],
     email: [null, [Validators.required, Validators.email]],
     address: [null, [Validators.required]],
     experience: [null, [Validators.required, Validators.min(0)]],
     requiredArea: [null, [Validators.required, Validators.min(1)]],
     requiredLocation: [null, [Validators.required]],
-    plantsType: ["Select plant type", [Validators.required]],
+    plantsType: ['Select plant type', [Validators.required]],
     numberOfColonies: [null, [Validators.required, Validators.min(1)]],
     workPlan: [null, [Validators.required, Validators.maxLength(3000)]],
     nationalId: [null, [Validators.required]],
-    documents: [null, [Validators.required]]
+    documents: [null, [Validators.required]],
   });
 
+  constructor(
+    private _FormBuilder: FormBuilder,
+    private _LicenceService: LicenceService
+  ) {}
+
   ngOnInit(): void {
-    this.apiaryForm.get('workPlan')?.valueChanges.subscribe(value => {
+    this.apiaryForm.get('workPlan')?.valueChanges.subscribe((value) => {
       const maxCharacters = 3000;
       this.charactersLeft = maxCharacters - (value?.length || 0);
     });
@@ -53,10 +62,19 @@ export class ApiaryLicenceComponent implements OnInit {
       formData.append('email', this.apiaryForm.get('email')?.value);
       formData.append('address', this.apiaryForm.get('address')?.value);
       formData.append('experience', this.apiaryForm.get('experience')?.value);
-      formData.append('requiredArea', this.apiaryForm.get('requiredArea')?.value);
-      formData.append('requiredLocation', this.apiaryForm.get('requiredLocation')?.value);
+      formData.append(
+        'requiredArea',
+        this.apiaryForm.get('requiredArea')?.value
+      );
+      formData.append(
+        'requiredLocation',
+        this.apiaryForm.get('requiredLocation')?.value
+      );
       formData.append('plantsType', this.apiaryForm.get('plantsType')?.value);
-      formData.append('numberOfColonies', this.apiaryForm.get('numberOfColonies')?.value);
+      formData.append(
+        'numberOfColonies',
+        this.apiaryForm.get('numberOfColonies')?.value
+      );
       formData.append('workPlan', this.apiaryForm.get('workPlan')?.value);
 
       const nationalIdFile = this.apiaryForm.get('nationalId')?.value;
@@ -70,12 +88,42 @@ export class ApiaryLicenceComponent implements OnInit {
       }
 
       this._LicenceService.requestLicence(formData).subscribe({
-        next: (res) => console.log(res),
-        error: (err) => console.error(err)
+        next: (res) => {
+          console.log(res);
+          this.showSuccessAlert(); // Show success message
+          this.apiaryForm.reset(); // Reset the form
+        },
+        error: (err) => {
+          console.error(err);
+          this.showErrorAlert(); // Show error message
+        },
       });
     } else {
       this.apiaryForm.markAllAsTouched();
     }
   }
 
+  showSuccessAlert() {
+    Swal.fire({
+      title: 'Success!',
+      text: 'Your request has been submitted successfully. We will contact you through email as soon as possible.',
+      icon: 'success',
+      confirmButtonText: 'OK',
+      customClass: {
+        confirmButton: 'btn btn-success',
+      },
+    });
+  }
+
+  showErrorAlert() {
+    Swal.fire({
+      title: 'Canceled!',
+      text: 'You can only submit one request every 30 days.',
+      icon: 'error',
+      confirmButtonText: 'Ok',
+      customClass: {
+        confirmButton: 'btn btn-primary',
+      },
+    });
+  }
 }
